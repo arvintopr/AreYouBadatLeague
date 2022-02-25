a07060c479b2e97b6f9a517d3970f0624ade9f17
arvintpr <artpratomo2@gmail.com>
2022-02-24 21:43:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function CallRiotApi(e) {
+    e.preventDefault();
+    console.log('You clicked button');
+    console.log(e.target.name.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={CallRiotApi}>
+        <label>
+              Name:
+              <input type="text" name="name" />
+        </label>
+              <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
